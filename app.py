eed99e1d38b5940e07b51e8a114d74ba2b8519af
Raju Ahmed Shetu <shetu2153@gmail.com>
2019-10-28 11:42:04 +0600

    added codes

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
-from flask import Flask
+from flask import Flask, request
 from flask import jsonify
+
 app = Flask(__name__)
 import os
+
 domain = os.getenv('domain')
+
+
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return jsonify(
+        dict(domain=domain, message='home', path=request.path)
+    )
+
 
 @app.route('/hello-world')
 def el():
     return jsonify(
-        dict(domain=domain,message='hello-world')
+        dict(domain=domain, message='hello-world', path=request.path)
     )
 
 
+@app.route('/<path:path_name>')
+def el_path(path_name):
+    return jsonify(
+        dict(domain=domain, path=request.path, message=path_name)
+    )
 
 
 if __name__ == '__main__':
